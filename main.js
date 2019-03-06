935b9566b4538966b80c485417741a0e80c044d5
etudeofmemories8698 <etudeofmemories8698@gmail.com>
2019-03-06 14:09:54 +0700

    add required funtionalities 4,5,6 (#2)

--- main.js
+++ main.js
@@ -1,24 +1,60 @@
+let generatedNumber = getRandomNumber();
+let turn = 10;
+
+let mostRecentGuess = document.querySelector(".alert");
+
+let guessRemaining = document.querySelector(".guessRemaining");
+
+
 function getRandomNumber() {
   return Math.floor(Math.random() * (100 + 1)) + 1;
 }
 
-let generatedNumber = getRandomNumber();
-
 function guessingNumber() {
   let guessedValue = document.querySelector("#guessedNumber");
   let guessedNumber = guessedValue.value;
+
   console.log(guessedNumber);
   console.log(generatedNumber);
-  guessedValue.value = "";
-  if(guessedNumber > generatedNumber) {
-    console.log("you're too HIGH");
-  } else if(guessedNumber < generatedNumber) {
-    console.log("you're too LOW");
-  } else if(guessedNumber = generatedNumber){
-    console.log("you're right");    
-  }
 
+  guessedValue.value = "";
+
+  if(turn > 1) {
+    turn = turn - 1;
+
+    if(guessedNumber > generatedNumber) {
+      mostRecentGuess.innerHTML = "you're too HIGH";
+      mostRecentGuess.classList.add("alert-danger");
+      mostRecentGuess.style.display = "block";
+    } else if(guessedNumber < generatedNumber) {
+      mostRecentGuess.innerHTML = "you're too LOW";
+      mostRecentGuess.classList.add("alert-danger");
+      mostRecentGuess.style.display = "block";
+    } else if(guessedNumber = generatedNumber) {
+      mostRecentGuess.innerHTML = "you're right";
+      mostRecentGuess.classList.remove("alert-danger");
+      mostRecentGuess.classList.add("alert-success");
+      mostRecentGuess.style.display = "block";
+    }
+    guessRemaining.innerHTML = `you have ${turn} turns left`;
+
+  } else {
+    guessRemaining.innerHTML = `you have ran out of turns`;
+  }
 } 
+
+function restartGame() {
+  turn = 10;
+  mostRecentGuess.innerHTML = "";
+  mostRecentGuess.classList.remove("alert-danger", "alert-success");
+  mostRecentGuess.style.display = "none";
+  guessRemaining.innerHTML = `you have ${turn} turns left`;
+  generatedNumber = getRandomNumber();
+}
+
 
 let guessBtn = document.querySelector("#guessBtn");
 guessBtn.addEventListener("click", guessingNumber);
+
+let restartBtn = document.querySelector("#restartBtn");
+restartBtn.addEventListener("click", restartGame);
